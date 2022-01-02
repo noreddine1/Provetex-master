8d16c25d425fbdb7b8158ef0e535b72ee050301f
nour <noreddine_devloper@hotmail.com>
2022-01-02 17:30:52 +0100

    rename F_CU to frm_create

--- Provetex/Item/frm_create.Designer.cs
+++ Provetex/Item/frm_create.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Provetex.Item
 {
-    partial class F_CU
+    partial class frm_create
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_CU));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_create));
             this.Button_back = new Bunifu.Framework.UI.BunifuThinButton2();
             this.Button_save = new Bunifu.Framework.UI.BunifuThinButton2();
             this.Textbox_phone = new Bunifu.Framework.UI.BunifuMaterialTextbox();
--- Provetex/Item/frm_create.cs
+++ Provetex/Item/frm_create.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Provetex.Item
 {
-    public partial class F_CU : Form
+    public partial class frm_create : Form
     {
-        public F_CU()
+        public frm_create()
         {
             InitializeComponent();
         }
